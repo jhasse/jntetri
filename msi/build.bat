09194cfcc66407cf499c53993ab6cb34d012c310
Jan Niklas Hasse <jhasse@gmail.com>
2015-01-13 19:30:49 +0100

    Update Windows installer to include all data files

--- msi/build.bat
+++ msi/build.bat
@@ -1,3 +1,7 @@
-candle.exe -nologo jntetri.wxs -ext WixUtilExtension
-light.exe -nologo -out JNTETRI.msi -ext WixUtilExtension -ext WixUIExtension jntetri.wixobj
-pause
+SET DataDir=..\data
+"%WIX%bin\heat.exe" dir "%DataDir%" -cg DataFiles -var env.DataDir -gg -scom -sreg -sfrag -srd -dr data -out data.wxs || goto :error
+"%WIX%bin\candle.exe" jntetri.wxs -ext WixUtilExtension || goto :error
+"%WIX%bin\candle.exe" data.wxs -ext WixUtilExtension || goto :error
+"%WIX%bin\light.exe" -out JNTETRI.msi -ext WixUtilExtension -ext WixUIExtension jntetri.wixobj data.wixobj || goto :error
+
+:error
