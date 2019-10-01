9616f4f028716e1d424ec13884d8d45e398604d6
Jan Niklas Hasse <jhasse@bixense.com>
2019-10-01 10:42:36 +0200

    Fix -Wunused-parameter

--- src/NetworkControl.cpp
+++ src/NetworkControl.cpp
@@ -6,13 +6,13 @@ NetworkControl::NetworkControl(std::shared_ptr<Socket> socket) : socket(std::mov
 	socket->Receive([this](std::string buf) { handleReceive(buf); });
 }
 
-bool NetworkControl::step(const std::function<void(ControlType)>& set) {
+bool NetworkControl::step(const std::function<void(ControlType)>& /*set*/) {
 	socket->Step();
 	// TODO
 	return false;
 }
 
-void NetworkControl::handleReceive(std::string buf) {
+void NetworkControl::handleReceive(std::string /*buf*/) {
 	// TODO
 	socket->Receive([this](std::string buf) { handleReceive(buf); });
 }
