e8ef7823136483f7175e1d24b15d831c544f22b8
Jan Niklas Hasse <jhasse@bixense.com>
2023-10-05 15:08:42 +0200

    Add server.py for local web testing

---
+++ web/server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import sys
+
+class CustomHandler (SimpleHTTPRequestHandler):
+	def end_headers (self):
+		self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+		self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+		SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+	test(CustomHandler, HTTPServer, port=8000)
